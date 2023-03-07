575142c639174ee575d5b1aed8aaf115c36e30ca
Антон Щерба <Tony.Shcherba@gmail.com>
2023-03-07 10:46:48 +0300

    perf: change lang to ru & optimize popup call

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import Glide from '@glidejs/glide';
 const glideSet: Partial<Glide.Options> = {
   type: 'carousel',
   focusAt: 'center',
-  startAt: 0,
   perView: 3,
   gap: 16,
   autoplay: 5000,
@@ -20,44 +19,36 @@ const glideSet: Partial<Glide.Options> = {
   },
 };
 
-const glide1 = new Glide('#glide-1', glideSet).mount();
-
-const glide2 = new Glide('#glide-2', glideSet).mount();
-
-[...document.getElementsByClassName('product-card')].map((openPopupBtn) => {
-  let flag = 1;
-  openPopupBtn?.addEventListener(
-    'mousedown',
-    function () {
-      flag = 0;
-    },
-    false
-  );
-  openPopupBtn?.addEventListener(
-    'mousemove',
-    function () {
-      flag = 1;
-    },
-    false
-  );
-  openPopupBtn?.addEventListener(
-    'mouseup',
-    function () {
-      if (flag === 0) {
-        popup?.classList.add('show');
-        glide1.pause();
-        glide2.pause();
-      }
-    },
-    false
-  );
-});
+const glide1 = new Glide('#glide-1', glideSet);
+const glide2 = new Glide('#glide-2', glideSet);
 
 const closePopupBtn = document.querySelector('.close-popup');
 const popup = document.getElementById('popup');
+const glidesWrap = document.getElementById('glides-wrap');
+
+let itMove = true;
+
+glidesWrap?.addEventListener('mousedown', (e) => {
+  if ((e.target as HTMLDivElement).closest('.product-card')) itMove = false;
+});
+
+glidesWrap?.addEventListener('mousemove', (e) => {
+  if ((e.target as HTMLDivElement).closest('.product-card')) itMove = true;
+});
+
+glidesWrap?.addEventListener('mouseup', (e) => {
+  if ((e.target as HTMLDivElement).closest('.product-card')) {
+    if (itMove) return;
+    popup?.classList.add('show');
+    document.body.style.overflow = 'hidden';
+    glide1.pause();
+    glide2.pause();
+  }
+});
 
 closePopupBtn?.addEventListener('click', () => {
   popup?.classList.remove('show');
+  document.body.style.overflow = '';
   glide1.play();
   glide2.play();
 });
@@ -65,9 +56,11 @@ closePopupBtn?.addEventListener('click', () => {
 window.addEventListener('click', (e) => {
   if (e.target === popup) {
     popup?.classList.remove('show');
+    document.body.style.overflow = '';
     glide1.play();
     glide2.play();
   }
 });
 
-console.log('Hello, world!');
+glide1.mount();
+glide2.mount();
